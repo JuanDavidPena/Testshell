bba0832334df195a071ade6048c9df47491f3b2d
JuanDavidPena <j_david0915@hotmail.com>
2021-08-17 23:13:54 +0000

    test

---
+++ prompt.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * prompt - Display prompt
+ */
+void prompt(void)
+{
+	PRINTER("$ ");
+}
+/**
+ * print_error - Display Error Based on Command and How Many Time Shell Looped
+ * @input:User Input
+ * @counter:count loop
+ * @argv:Program
+ * Return: void
+ */
+void print_error(char *input, int counter, char **argv)
+{
+	char *er;
+
+	PRINTER(argv[0]);
+	PRINTER(": ");
+	er = _itoa(counter);
+	PRINTER(er);
+	free(er);
+	PRINTER(": ");
+	PRINTER(input);
+	PRINTER(": not found\n");
+}
